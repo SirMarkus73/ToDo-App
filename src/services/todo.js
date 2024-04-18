cd08db8727b5e4cb34ba49e8eb8ffdfaf1d848c4
Markus <marcosbh2002bh@gmail.com>
2024-04-18 21:22:52 +0200

    Se ha mejorado el responsive

--- src/services/todo.js
+++ src/services/todo.js
@@ -29,7 +29,7 @@ formChildS.button.addEventListener("click", () => {
 
 function addAppTask(taskText, append = true) {
   if (taskText != "") {
-    showTasks.innerHTML += `<div class='flex flex-row gap-3 justify-between transition ease-in-out delay-75 dark:bg-dark-950 dark:hover:bg-dark-500 bg-bright-500 hover:bg-bright-850 hover:text-white hover:-translate-y-1 hover:scale-105 duration-300 border-2 border-slate-900'><select class="text-sm bg-bright-500 dark:bg-dark-500"><option value="1" class="text-xs">Completada</option><option value="0.5" class="text-xs">A medias</option><option value="0" class="text-xs" selected>Sin Completar</option></select><p>${taskText}</p><button><i class='bx bx-task-x'></i></button></div>`;
+    showTasks.innerHTML += `<div class='flex text-center min-w-fit flex-col lg:flex-row gap-3 justify-between transition ease-in-out delay-75 dark:bg-dark-950 dark:hover:bg-dark-500 bg-bright-500 hover:bg-bright-850 hover:text-white hover:-translate-y-1 hover:scale-105 duration-300 border-2 border-slate-900'><select class="text-sm bg-bright-500 dark:bg-dark-500"><option value="1" class="text-xs">Completada</option><option value="0.5" class="text-xs">A medias</option><option value="0" class="text-xs" selected>Sin Completar</option></select><p>${taskText}</p><button><i class='bx bx-task-x'></i></button></div>`;
 
     if (append) {
       localTasks.push(taskText);
