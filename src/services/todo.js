4141b59dd1930ffad749914bf1b9bb994657c7a9
Markus <marcosbh2002bh@gmail.com>
2024-04-18 18:24:38 +0200

    Ahora se guardan los todos en el cliente

--- src/services/todo.js
+++ src/services/todo.js
@@ -1,30 +1,62 @@
-const form = document.querySelector("form-no-reload");
+let showTasks = document.getElementById("tareas");
+
+const getTasks = () => {
+  return localStorage.getItem("tasks").split(",");
+};
+
+let localTasks = getTasks();
+
+const updateTasks = (tasks) => {
+  localStorage.setItem("tasks", tasks);
+};
+
+localTasks.forEach((task) => {
+  addAppTask(task, false);
+});
+
+const form = document.querySelector("form");
 
 const formChildS = {
   inputText: form.querySelector("input"),
   button: form.querySelector("button"),
 };
 
-let tareas = document.getElementById("tareas");
-
 formChildS.button.addEventListener("click", () => {
   let text = formChildS.inputText.value;
-  console.log(text);
 
-  if (text != "") {
-    tareas.innerHTML += `<div class='flex flex-row gap-3 justify-between'"><p>${text}</p><button><i class='bx bx-task-x'></i></button></div>`;
+  addAppTask(text);
+});
+
+function addAppTask(taskText, append = true) {
+  if (taskText != "") {
+    showTasks.innerHTML += `<div class='flex flex-row gap-3 justify-between'><p>${taskText}</p><button><i class='bx bx-task-x'></i></button></div>`;
+
+    if (append) {
+      localTasks.push(taskText);
+
+      updateTasks(localTasks);
+
+      console.log(localStorage.getItem("tasks"));
+    }
   }
 
   update_remove();
-});
+}
 
 function update_remove() {
-  tareas.querySelectorAll("div").forEach((tarea) => {
+  showTasks.querySelectorAll("div").forEach((tarea) => {
     let deleteButton = tarea.querySelector("button");
+    let taskContent = tarea.querySelector("p");
+
     deleteButton.addEventListener("click", () => {
       tarea.remove();
+      let index = localTasks.indexOf(taskContent.innerHTML);
+
+      console.log(index);
+
+      localTasks.splice(index, 1);
+
+      updateTasks(localTasks);
     });
   });
 }
-
-update_remove();
